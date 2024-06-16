87b68bacf48773192cc04560d59985951c09239f
adilavirk <adilavirk0@gmail.com>
2024-06-16 18:16:18 +0500

    fixed bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
-// export const config = {
-//   matcher: ["/cart", "/wishlist", "/success"],
-// };
+export const config = {
+  matcher: ["/cart", "/wishlist", "/success"],
+};
